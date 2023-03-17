9ac1e41d12cf4dcb68879a9b443239806b7b1537
Nathan Moes <nathanmoes7@gmail.com>
2023-03-17 11:53:39 -0700

    formatting list

--- pages/Books.js
+++ pages/Books.js
@@ -12,6 +12,7 @@ const Books = () => {
   const [books, setBooks] = React.useState([]);
   const [bookTitle, setBookTitle] = React.useState("");
   const [bookPages, setBookPages] = React.useState("");
+  const color = "000000";
 
   const resetButtonHandler = () => {
     setBooks([]);
@@ -31,17 +32,33 @@ const Books = () => {
   };
 
   const addBookHandler = () => {
-    setBooks((prev) => {
-      return [...prev, { title: bookTitle, pages: bookPages }];
-    });
+    if (bookTitle.length > 1) {
+      setBooks((prev) => {
+        return [...prev, { title: bookTitle, pages: bookPages }];
+      });
+      setBookPages("");
+      setBookTitle("");
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text>Books!</Text>
-      <Button title="button" onPress={booksButtonHandler}></Button>
-      <Button title="reset" onPress={resetButtonHandler}></Button>
-      <Button title="add book" onPress={addBookHandler}></Button>
+      <Button
+        title="button"
+        onPress={booksButtonHandler}
+        color={`#${color}`}
+      ></Button>
+      <Button
+        title="reset"
+        onPress={resetButtonHandler}
+        color={`#${color}`}
+      ></Button>
+      <Button
+        title="add book"
+        onPress={addBookHandler}
+        color={`#${color}`}
+      ></Button>
       <TextInput
         style={styles.input}
         onChangeText={(newText) => setBookTitle(newText)}
@@ -56,7 +73,9 @@ const Books = () => {
       <FlatList
         data={books}
         renderItem={({ item }) => (
-          <Text style={styles.books}>{item.title}</Text>
+          <Text
+            style={styles.books}
+          >{`title: ${item.title} \n pages: ${item.pages}`}</Text>
         )}
       />
     </View>
@@ -72,15 +91,18 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    margin: 12,
+    marginTop: 10,
     borderWidth: 1,
     padding: 10,
+    minWidth: 300,
   },
   books: {
     minHeight: 30,
     minWidth: 30,
     margin: 10,
     padding: 10,
+    borderWidth: 1,
+    borderColor: "#09ad35",
   },
 });
 
